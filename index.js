afa637f2a61e0b9f4df792a5ecbb32be9704354e
kle-pra <klemen.pravdic@gmail.com>
2019-10-24 22:55:17 +0200

    basic chat

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const http = require('http')
+const socketIo = require('socket.io');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+const server = http.createServer(app);
+const io = socketIo(server);
+const users = new Map();
+
+io.on('connection', client => {
+  client.on('chatInfo', data => {
+    client.join(data.room);
+    users.set(client.id, data);
+    client.to(data.room).broadcast.emit('chatInfo', { message: `${data.username} connected.` });
+    io.to(data.room).emit('users', Array.from(users.values()).filter(user => user.room === data.room));
+  });
+
+  client.on('chatMessage', data => {
+    console.log(data.message);
+    const { room } = users.get(client.id);
+    io.to(room).emit('chatMessage', data);
+  });
+
+  client.on('disconnect', () => {
+    try {
+      const { username, room } = users.get(client.id);
+      users.delete(client.id);
+      io.to(room).emit('chatInfo', { message: `${username} disconnected.` });
+      io.to(room).emit('users', Array.from(users.values()));
+    } catch { }
+  });
+
+  client.on('image', imageData => {
+    try {
+      const { username, room } = users.get(client.id);
+      io.to(room).emit('image', imageData);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(3000, () => {
+  console.log(`App listening on port ${port}`);
+
+})
---
+++ public/main.js
@@ -0,0 +1,97 @@
+
+const userData = document.querySelector('.user-data');
+const usernameInput = document.getElementById('usernameInput');
+const roomInput = document.getElementById('roomInput');
+const joinBtn = document.getElementById('joinBtn');
+const chat = document.querySelector('.chat');
+const chatArea = document.getElementById('chatArea');
+const usersInfoArea = document.getElementById('usersInfoArea');
+const roomTitle = document.getElementById('roomTitle');
+const sendBtn = document.getElementById('sendBtn');
+const msgInput = document.getElementById('msgInput');
+const imageCard = document.querySelector('.card.image');
+const captureVideoButton = document.querySelector('#captureBtn');
+const screenshotButton = document.querySelector('#takeScreenshotBtn');
+const sendImageBtn = document.querySelector('#sendImageBtn');
+const imgPreview = document.querySelector('#imageArea #preview');
+const video = document.querySelector('#imageArea video');
+const canvas = document.querySelector('canvas');
+
+let username = '';
+let room = '';
+let socket = null;
+
+const join = () => {
+  username = usernameInput.value || `anonymous${Math.ceil(Math.random() * 1000)}`;
+  room = roomInput.value || 'general';
+  chat.style.display = 'flex';
+  userData.style.display = 'none';
+  roomTitle.innerText = `${username} @ ${room}`;
+
+  socket = io(); // == io('http://localhost:3000');
+  socket.emit('chatInfo', { username, room });
+  socket.on('chatMessage', data => chatArea.innerHTML += `<strong>${data.username}</strong>:  ${data.message}<br>`);
+  socket.on('chatInfo', data => chatArea.innerHTML += `<em>${data.message}</em><br>`);
+  socket.on('users', users => {
+    usersInfoArea.innerHTML = '';
+    users.forEach(user => usersInfoArea.innerHTML += '<em>' + user.username + '</em><br>');
+  });
+  socket.on('image', data => {
+    chatArea.innerHTML += `<strong>${data.username}</strong>: <img height="150px" src="${data.image}"><br>`;
+  });
+};
+
+const send = event => {
+  if (event.keyCode == 13 || !event.keyCode) {
+    socket.emit('chatMessage', { username, message: msgInput.value, room });
+    msgInput.value = '';
+    chatArea.scrollTop = chatArea.scrollHeight;
+  }
+};
+
+const sendImage = () => {
+  imageCard.style.display = 'none';
+  imgPreview.style.display = 'none';
+  const dataURL = canvas.toDataURL("image/png");
+  socket.emit('image', { username, room, image: dataURL });
+  captureVideoButton.style.display = 'initial';
+  sendImageBtn.style.display = 'none';
+};
+
+const captureVideo = () => {
+  imageCard.style.display = 'initial';
+  screenshotButton.style.display = 'initial';
+  captureVideoButton.style.display = 'none';
+  video.style.display = 'block';
+  // canvas.style.display = 'none';
+
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+      screenshotButton.disabled = false;
+      video.srcObject = stream;
+    })
+    .catch(console.log);
+};
+
+const takeScreenshot = () => {
+
+
+  const vRatio = (canvas.height / video.videoHeight) * video.videoWidth;
+
+  canvas.getContext('2d').drawImage(video, 0, 0, vRatio, canvas.height);
+  imgPreview.width = vRatio;
+  // imgPreview.height = video.videoHeight;
+  imgPreview.src = canvas.toDataURL('image/webp');
+  video.style.display = 'none';
+  screenshotButton.style.display = 'none';
+  sendImageBtn.style.display = 'initial';
+  imgPreview.style.display = 'initial';
+};
+
+
+sendBtn.addEventListener('click', send);
+msgInput.addEventListener('keyup', send);
+joinBtn.addEventListener('click', join);
+captureVideoButton.addEventListener('click', captureVideo);
+sendImageBtn.addEventListener('click', sendImage);
+screenshotButton.addEventListener('click', takeScreenshot);
